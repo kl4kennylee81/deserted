f68e5518d6b946cc2c5aa128cb9ee3e429f67b78
Kenneth Lee <kl488@cornell.edu>
2016-05-09 15:12:48 -0400

    change sizeof canvas

--- desktop/src/edu/cornell/gdiac/ailab/desktop/DesktopLauncher.java
+++ desktop/src/edu/cornell/gdiac/ailab/desktop/DesktopLauncher.java
@@ -35,8 +35,8 @@ public class DesktopLauncher {
 	 */ 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width  = 800;
-		config.height = 600;
+		config.width  = 1280;
+		config.height = 720;
 		new LwjglApplication(new GDXRoot(), config);
 	}
 }
