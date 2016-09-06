302c795aaaa8db5aea5cf50818a0fd0bf74d9293
Ishaan Jhaveri <iaj8@cornell.edu>
2016-09-06 18:27:11 -0400

    added defense stat

--- core/src/edu/cornell/gdiac/ailab/DraftController.java
+++ core/src/edu/cornell/gdiac/ailab/DraftController.java
@@ -170,8 +170,8 @@ public class DraftController {
 		Characters list = new Characters();
 		BossCharacter c1 = (BossCharacter) draftScreen.getCharacter(draftScreen.player1Characters[0]);
 		BossCharacter c2 = (BossCharacter) draftScreen.getCharacter(draftScreen.player2Characters[0]);
-		Character parent1 = new Character(2000, c1.texture, c1.icon, c1.animation, "Ishaan", 10, 10, Color.BLUE, 0, 0, new Action[]{},0);
-		Character parent2 = new Character(2001, c2.texture, c2.icon, c2.animation, "Ishaan", 10, 10, Color.YELLOW, 0, 0, new Action[]{},0);
+		Character parent1 = new Character(2000, c1.texture, c1.icon, c1.animation, "Ishaan", 10, 10, Color.BLUE, 0, 0, 0, new Action[]{},0);
+		Character parent2 = new Character(2001, c2.texture, c2.icon, c2.animation, "Ishaan", 10, 10, Color.YELLOW, 0, 0, 0, new Action[]{},0);
 		int health1 = 0;
 		for(int i = 0; i < draftScreen.player1Characters.length; i++){
 			BossCharacter c = (BossCharacter) draftScreen.getCharacter(draftScreen.player1Characters[i]);
